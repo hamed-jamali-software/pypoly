b5a6ff2bc152da3c2e531a79632e70556b9cf862
hamed-jamali-software <hamed.jamali.software@gmail.com>
2024-07-13 14:32:04 +0200

    added account models

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,34 @@
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 from django.urls import path
 from . import views
+from .views import logout_view
+
+from .views import (
+    HomeView,
+    PostUpdateView,
+    PostDeleteView,
+    AddPostView,
+    SignupView,
+    LoginInterfaceView,
+    AuthorizedView,
+    PostListView,
+
+)
+
+
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', HomeView.as_view(), name='home'),
     path('about/', views.about, name='blog-about'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('add_post/', views.add_post, name='add_post'),
-    path('register/', views.register, name='register'),
+    path('add_post/', AddPostView.as_view(), name='add_post'),
+
+
+    # User Authentication
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('login/', LoginInterfaceView.as_view(), name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('authorized/', AuthorizedView.as_view(), name='authorized'),
+
 ]
